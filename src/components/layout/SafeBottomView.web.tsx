fb2b3c509c49f84c43bef2573e8ec3737aea0026
vitorbmulford <vitorbmulford@gmail.com>
2026-07-12 13:14:40 -0300

    forcando padding 0

--- src/components/layout/SafeBottomView.web.tsx
+++ src/components/layout/SafeBottomView.web.tsx
@@ -1,7 +1,20 @@
 import { type ViewProps, type ViewStyle, View } from 'react-native';
 
 const safeBottomStyle = {
+  bottom: 0,
+  flexBasis: 'auto',
+  flexGrow: 0,
+  flexShrink: 0,
+  height: 'auto',
+  left: 0,
+  marginBottom: 0,
+  maxHeight: 'none',
+  minHeight: 0,
   paddingBottom: 0,
+  position: 'fixed',
+  right: 0,
+  width: '100%',
+  zIndex: 1000,
 } as unknown as ViewStyle;
 
 export function SafeBottomView({ style, ...props }: ViewProps) {
